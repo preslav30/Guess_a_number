9637602dcd94692d3ab3f41f294123082f77e78d
preslav30 <112109330+preslav30@users.noreply.github.com>
2022-09-20 23:44:48 +0300

    Create guess_a_number.py

---
+++ guess_a_number.py
@@ -0,0 +1,15 @@
+import random
+
+def guess_number(guess):
+    number_chosen_by_computer = random.randint(1, 100)
+    print(number_chosen_by_computer)
+    while True:
+        if guess == number_chosen_by_computer:
+            return "You have guessed the number!"
+        elif guess < number_chosen_by_computer:
+            print("Too low. Guess again!")
+        elif guess > number_chosen_by_computer:
+            print("Too high. Guess again!")
+        guess = int(input())
+
+print(guess_number(guess=int(input("Guess a number from 1 to 100:\n"))))
